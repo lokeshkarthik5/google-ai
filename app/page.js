2daa36b2edcee9e32dd7db62ae495465e53a92f3
LokeshKarthik5 <34629208+lokeshkarthik5@users.noreply.github.com>
2024-05-02 09:57:45 +0530

    finale

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Logo from "@/public/Logo.png"
 import Image from "next/image";
+import Gemini from "@/public/gemini.png"
 
 const monstserrat = Montserrat({
   weight: "700",
@@ -21,13 +22,19 @@ export default function Home() {
         <div className="flex flex-col items-center">
         <Button className="mt-10"><Link href="/dashboard">Get started</Link></Button>
         <h2 className="text-blue-700 mr-0 mt-5">Powered by Google AI</h2>
+        <Image src={Gemini} alt="Gemini" className="w-[300px] h-[80px] mt-5" />
         </div>
         <div className="mt-20 font-semibold">
-        <h2>Just upload your contract in <span className="text-blue-700">PDF</span> Format</h2>
-        <h2>Get Insights about document and we'll even figure out better terms for you</h2>
+        <h2>We use the latest gemini pro model <span className="text-blue-700">To</span> generate</h2>
+        <h2>We've given instruction to give you the best result ever</h2>
         </div>
       </div>
-      <div>
+      <div className="mb-12 text-xl">
+        <h1>What you can do with our platform:</h1>
+        <h1>Upload your document in PDF form</h1>
+        <h1>Get a summary of the document</h1>
+        <h1>Our site gives you insights and even suggests you better terms</h1>
+
       </div>
     </main>
   );
