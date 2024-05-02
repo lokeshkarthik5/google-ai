981aa233f46b7acaa7906fbb2d5f49d43a29dc55
LokeshKarthik5 <34629208+lokeshkarthik5@users.noreply.github.com>
2024-05-03 00:10:26 +0530

    v1.0

--- app/page.js
+++ app/page.js
@@ -20,16 +20,16 @@ export default function Home() {
         <h1 className={cn("text-3xl w-[550px] mt-[50px]",monstserrat.className)}>Legal Contract Summarizer and <span className="text-blue-700">Analyzer</span> </h1>
         </div>
         <div className="flex flex-col items-center">
-        <Button className="mt-10"><Link href="/dashboard">Get started</Link></Button>
+        <Button className="mt-[70px]"><Link href="/dashboard">Get started</Link></Button>
         <h2 className="text-blue-700 mr-0 mt-5">Powered by Google AI</h2>
-        <Image src={Gemini} alt="Gemini" className="w-[300px] h-[80px] mt-5" />
+        <Image src={Gemini} alt="Gemini" className="w-[300px] h-[80px] mt-10" />
         </div>
-        <div className="mt-20 font-semibold">
+        <div className="mt-[100px] font-semibold">
         <h2>We use the latest gemini pro model <span className="text-blue-700">To</span> generate</h2>
-        <h2>We've given instruction to give you the best result ever</h2>
+        <h2>We tailored the model to provide you the best result</h2>
         </div>
       </div>
-      <div className="mb-12 text-xl">
+      <div className="mb-12 text-xl mt-[100px]">
         <h1>What you can do with our platform:</h1>
         <h1>Upload your document in PDF form</h1>
         <h1>Get a summary of the document</h1>
